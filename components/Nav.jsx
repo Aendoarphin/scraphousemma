9cc93e8a366ae714c4c628fea3f0b5c77fa9edb9
ArhonPineda <aronp2000@gmail.com>
2024-02-04 23:48:57 -0600

    modify appearance

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
@@ -37,12 +37,28 @@ const Nav = () => {
       className={`fixed top-0 right-0 left-0 ${
         visible
           ? "translate-y-0 transition-transform"
-          : "-translate-y-24 transition-transform"
+          : "-translate-y-96 transition-transform"
       }`}
     >
+      {isOpen && (
+        <div
+          className={`border-b border-black border-opacity-5 dark:border-white dark:border-opacity-5 flex flex-row justify-evenly  ${
+            visible
+              ? "translate-y-0 transition-transform"
+              : "-translate-y-20 transition-transform"
+          }`}
+        >
+          <a href="" className="p-2">
+            Dashboard
+          </a>
+          <a href="" className="p-2">
+            About
+          </a>
+        </div>
+      )}
       <div className="px-8 py-4 flex items-center justify-between">
         <a id="brand" className="brand text-6xl px-4 cursor-pointer">
-          S<span className="text-main">H</span>
+          S<span className="hidden lg:inline">crap</span><span className="text-main">H<span className="hidden lg:inline">ouse</span></span>
         </a>
         <div id="links" className="flex flex-wrap items-center gap-6 text-sm">
           <FontAwesomeIcon icon={faBars} onClick={handleMenuClick} />
@@ -52,23 +68,6 @@ const Nav = () => {
           <ThemeToggle />
         </div>
       </div>
-      {isOpen && (
-        <div
-          className={`flex flex-row justify-evenly dark:text-white bg-white bg-opacity-5 text-white shadow-black shadow-inner-soft ${
-            visible
-              ? "translate-y-0 transition-transform"
-              : "-translate-y-20 transition-transform"
-          }`}
-        >
-          <a href="" className="p-2">
-            Dashboard
-          </a>
-          <div className="border border-black opacity-10"></div>
-          <a href="" className="p-2">
-            About
-          </a>
-        </div>
-      )}
     </div>
   );
 };
