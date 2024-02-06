bbe924127c5cff7e53e886c03f86413719b2e610
ArhonPineda <aronp2000@gmail.com>
2024-02-05 21:44:15 -0600

    refactor animation and formatting

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,27 +1,28 @@
-'use client'
+"use client";
+// Font Awesome assets
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+// Hooks
+import { useState, useEffect } from "react";
+// Components
 import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
-  // Toggle nav links
   const [isOpen, setIsOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setIsOpen(!isOpen);
-  };
-  // Show/hide nav bar on scroll
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
 
+  // Expand/collapse links
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Show/hide navbar on scroll
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       const isScrollingDown = currentScrollPos < prevScrollPos;
-
       setPrevScrollPos(currentScrollPos);
-
       setVisible(isScrollingDown || currentScrollPos < 10);
     };
 
@@ -40,28 +41,37 @@ const Nav = () => {
           : "-translate-y-96 transition-transform"
       }`}
     >
-      {isOpen && (
-        <div
-          className={`border-b border-black border-opacity-5 dark:border-white dark:border-opacity-5 flex flex-row justify-evenly  ${
-            visible
-              ? "translate-y-0 transition-transform"
-              : "-translate-y-20 transition-transform"
-          }`}
-        >
-          <a href="" className="p-2">
-            Dashboard
-          </a>
-          <a href="" className="p-2">
-            About
-          </a>
+      <div
+        className={`transition-all ${
+          isOpen ? "h-20 opacity-100" : "h-0 opacity-0"
+        }`}
+      >
+        <div className="border-b border-black border-opacity-5 dark:border-white dark:border-opacity-5 flex flex-row justify-evenly">
+          {isOpen && (
+            <>
+              <a href="" className="p-2">
+                Dashboard
+              </a>
+              <a href="" className="p-2">
+                About
+              </a>
+            </>
+          )}
         </div>
-      )}
+      </div>
       <div className="px-8 py-4 flex items-center justify-between">
         <a id="brand" className="brand text-6xl px-4 cursor-pointer">
-          S<span className="hidden lg:inline">crap</span><span className="text-main">H<span className="hidden lg:inline">ouse</span></span>
+          S<span className="hidden lg:inline">crap</span>
+          <span className="text-main">
+            H<span className="hidden lg:inline">ouse</span>
+          </span>
         </a>
         <div id="links" className="flex flex-wrap items-center gap-6 text-sm">
-          <FontAwesomeIcon icon={faBars} onClick={handleMenuClick} />
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={handleMenuClick}
+            className="hover:cursor-pointer"
+          />
           <a href="">
             <FontAwesomeIcon icon={faUser} />
           </a>
