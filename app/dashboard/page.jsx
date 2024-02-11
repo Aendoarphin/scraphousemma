7c4f5af68416331806afcb4a04c1eeb8ebc11882
Arhon <aronp2000@gmail.com>
2024-02-10 23:49:14 -0600

    add new components

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import Panel from "@/components/Panel";
+
+const Dashboard = () => {
+    return (
+      <>
+        <div className="border border-main pt-24 p-8 h-screen">
+            <Panel/>
+        </div>
+      </>
+    )
+  }
+  
+  export default Dashboard;
---
+++ components/Panel.jsx
@@ -0,0 +1,16 @@
+const Panel = () => {
+	return (
+		<>
+			<div className="bg-grey rounded-t-md h-min p-8 flex items-center mx-auto max-w-full overflow-hidden flex-col">
+				<div>img</div>
+        <div>img</div>
+        <div>img</div>
+			</div>
+			<div className="rounded-b-md mt-1 bg-main p-2 flex mx-auto max-w-full overflow-hidden">
+        <h1 className="m-auto font-extrabold">Button</h1>
+      </div>
+		</>
+	);
+};
+// left off here
+export default Panel;
