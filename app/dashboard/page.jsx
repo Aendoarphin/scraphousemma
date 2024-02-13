d61b65d229eec7134334722cdced5ca7635b88c7
ArhonPineda <aronp2000@gmail.com>
2024-02-12 18:57:59 -0600

    .

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,8 +3,10 @@ import Panel from "@/components/Panel";
 const Dashboard = () => {
     return (
       <>
-        <div className="border border-main pt-24 p-8 h-screen">
-            <Panel/>
+        <div className="border border-main pt-24 p-8 h-screen flex flex-col gap-4">
+            <Panel buttonLabel={"News"}/>
+            <Panel buttonLabel={"Rankings"}/>
+            <Panel buttonLabel={"Event"}/>
         </div>
       </>
     )
--- components/Panel.jsx
+++ components/Panel.jsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
 
-const Panel = () => {
+const Panel = (props) => {
 	return (
 		<>
-			<div className="bg-dark-grey rounded-t-md h-min p-8 flex gap-8 max-w-full overflow-hidden flex-col">
-				<div className="justify-around flex gap-4">
-					<Image
-						src={'/images/dev-thumbnail.jpg'}
-						alt="news thumbnail"
-						height={100}
-						width={100}
-						className=" object-cover"
-					/>
-					<div className="flex flex-col border-y-[1px] py-1">
-						<h1 className="text-lg font-extrabold">This is a heading for a news article headline</h1>
-						<p className="text-sm opacity-25">Jan 1, 2023 | Source</p>
+			<div>
+				<div className="bg-dark-grey rounded-t-md h-min p-8 flex gap-8 max-w-full overflow-hidden flex-col">
+					<div className="justify-around flex gap-4">
+						<Image
+							src={'/images/dev-thumbnail.jpg'}
+							alt="news thumbnail"
+							height={100}
+							width={100}
+							className=" object-cover"
+						/>
+						<div className="flex flex-col py-1">
+							<h1 className="text-lg font-extrabold">This is a heading for a news article headline</h1>
+							<p className="text-sm opacity-25">Jan 1, 2023 | Source</p>
+						</div>
 					</div>
 				</div>
-				
+				<div className="hover:-translate-y-1 active:contrast-50 rounded-b-md mt-1 bg-main p-2 flex max-w-full overflow-hidden">
+					<h1 className="m-auto font-extrabold">{props.buttonLabel}</h1>
+				</div>
 			</div>
-			<div className="hover:-translate-y-1 active:contrast-50 rounded-b-md mt-1 bg-main p-2 flex mx-auto max-w-full overflow-hidden">
-        <h1 className="m-auto font-extrabold">News</h1>
-      </div>
 		</>
 	);
 };
