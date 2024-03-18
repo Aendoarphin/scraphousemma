e065684b04021fdd0faa4cdec8d5683ab6d3b26c
Arhon <aronp2000@gmail.com>
2024-03-18 17:18:27 -0500

    edit footer style

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 	let currentYear = new Date();
 	return (
 		<>
-			<div className="text-xs p-10 flex items-center justify-between">
+			<div className="text-xs flex items-center justify-between mt-auto bg-gray-90 text-center p-10">
 				<p>&copy; {currentYear.getFullYear()} ScrapHouse</p>
 				<div className="flex gap-2">
 					<FontAwesomeIcon icon={faLinkedin} />
