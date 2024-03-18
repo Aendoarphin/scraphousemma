e91600c83bdbed0b1ce3ec769977c849e23456af
Arhon <aronp2000@gmail.com>
2024-03-18 17:14:00 -0500

    remove external components

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,24 +1,16 @@
 "use client";
-import Form from "@/components/ui/Form";
+// Components
 import SavedItems from "@/components/ui/dashboard/SavedItems";
 import User from "@/components/ui/dashboard/User";
 
 const DashboardHome = () => {
 	return (
 		<>
-			<div className="h-full flex flex-col justify-center gap-4 items-start">
-				{false ? (
-					<div className="size-full">
-						<User />
-						<SavedItems />
-					</div>
-				) : (
-					<div className="flex flex-row p-4 border size-full items-center justify-evenly">
-						<Form type={"login"} />
-						<p>OR</p>
-						<Form type={"register"} />
-					</div>
-				)}
+			<div className="h-full flex flex-col justify-center gap-4 items-start rounded-md">
+				<div className="size-full">
+					<User />
+					<SavedItems />
+				</div>
 			</div>
 		</>
 	);
