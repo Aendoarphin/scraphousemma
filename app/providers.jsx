c04589cffd8ee87b39762fe102ddbb3e6356f0f7
Arhon <aronp2000@gmail.com>
2024-03-18 17:10:03 -0500

    add docstring

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,7 +1,10 @@
 "use client";
-
 import { ThemeProvider } from "next-themes";
-
+/**
+ * A component that wraps the application with theme provider.
+ * @param {React.ReactNode} props.children - The child elements to be wrapped by the theme provider.
+ * @returns {JSX.Element} A JSX element representing the wrapped application with theme provider.
+ */
 export function Providers({ children }) {
 	return (
 		<ThemeProvider attribute="class">
