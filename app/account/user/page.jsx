c08d2730f32da104f1d15393a151e6cf64bd15fb
Arhon <aronp2000@gmail.com>
2024-03-18 17:10:43 -0500

    create account form

---
+++ app/account/user/page.jsx
@@ -0,0 +1,11 @@
+import AccountForm from "@/components/ui/AccountForm";
+
+const User = () => {
+	return (
+		<div className="flex justify-center">
+      <AccountForm/>
+    </div>
+	);
+};
+
+export default User;
