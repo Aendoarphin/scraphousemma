6744c88f09e759920f3be52cf0585a79bca650dc
Arhon <aronp2000@gmail.com>
2024-03-25 01:38:30 -0500

    create rankings template

--- app/dashboard/rankings/ufc/page.jsx
+++ app/dashboard/rankings/ufc/page.jsx
@@ -1,23 +1,36 @@
 import ItemFilter from "@/components/ui/ItemFilter";
-import React from "react";
+import { divisions, p4p } from "@/constants";
 
-const PageUfc = () => {
-  const gender = "Men";
-  const division = "P4P";
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-row justify-between">
-        <h1 className="font-heading">
-          {division}
-        </h1>
-        <ItemFilter initialValue="Men"/>
-      </div>
-      <div className="bg-dark-grey p-4 rounded-md">
-        ranks
-      </div>
-      
-    </div>
-  );
+let fighters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const FighterCard = () => {
+	return (
+		<div className="border">
+			<div className="border">Image</div>
+			<div className="border p-4">#RankNo Name</div>
+		</div>
+	);
+};
+
+const Rankings = (props) => {
+	return (
+		<div className="flex flex-col gap-4">
+			<h1 className="font-heading">Pound For Pound</h1>
+			<div className="bg-dark-grey p-4 font-heading rounded-md flex flex-wrap gap-4">
+				{fighters.map((value, index) => {
+					return <FighterCard key={index} />;
+				})}
+			</div>
+		</div>
+	);
+};
+
+const RankingsUfc = () => {
+	return (
+		<div className="">
+			<Rankings />
+		</div>
+	);
 }; // making rankings template
 
-export default PageUfc;
+export default RankingsUfc;
