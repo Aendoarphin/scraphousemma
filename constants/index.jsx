0317029efc8b02ee655f6433401acb1373e32f58
Arhon <aronp2000@gmail.com>
2024-03-25 01:38:09 -0500

    add new sample data

--- constants/index.jsx
+++ constants/index.jsx
@@ -1,244 +1,454 @@
 export const dashboardHomePanels = [
-  {
-    id: "news",
-    title: "News",
-    description: "Stay updated with the latest news and headlines.",
-    route: "dashboard/news",
-  },
-  {
-    id: "rankings",
-    title: "Rankings",
-    description: "Check out the latest rankings.",
-    route: "dashboard/rankings",
-  },
-  {
-    id: "schedule",
-    title: "Schedule",
-    description: "View upcoming events.",
-    route: "dashboard/schedule",
-  },
+	{
+		id: "news",
+		title: "News",
+		description: "Stay updated with the latest news and headlines.",
+		route: "dashboard/news",
+	},
+	{
+		id: "rankings",
+		title: "Rankings",
+		description: "Check out the latest rankings.",
+		route: "dashboard/rankings",
+	},
+	{
+		id: "schedule",
+		title: "Schedule",
+		description: "View upcoming events.",
+		route: "dashboard/schedule",
+	},
 ];
 
 export const sampleNewsData = [
-  {
-    articleId: 1,
-    name: "Topuria KOs Volkanovski to win the FW title",
-    publishedDate: "3 days ago",
-    source: "UFC",
-    image: "/images/thumbnail1.jpg",
-  },
-  {
-    articleId: 2,
-    name: "Pereira vs Hill announced to be main event of UFC 300",
-    publishedDate: "2 days ago",
-    source: "MMA Junkie",
-    image: "/images/thumbnail2.jpg",
-  },
-  {
-    articleId: 3,
-    name: "O'Malley targets Topuria to be his next fight",
-    publishedDate: "1 hour ago",
-    source: "ONE FC",
-    image: "/images/thumbnail3.jpg",
-  },
-  {
-    articleId: 4,
-    name: "Jones to fight Namajunas in 2024 in spite of torn pectorial",
-    publishedDate: "Yesterday",
-    source: "Sherdog",
-    image: "/images/thumbnail4.jpg",
-  },
-  {
-    articleId: 5,
-    name: "Royval defeats Moreno",
-    publishedDate: "2 days ago",
-    source: "MMA Frenzy",
-    image: "/images/thumbnail5.jpg",
-  },
-  {
-    articleId: 6,
-    name: "Rodtang to train with 10th Planet Jiu-Jitsu",
-    publishedDate: "1 hour ago",
-    source: "ONE FC",
-    image: "/images/thumbnail6.jpg",
-  },
-  {
-    articleId: 7,
-    name: "GSP Returns: Georges St-Pierre Confirms Comeback Fight Against Khabib Nurmagomedov",
-    publishedDate: "1 hour ago",
-    source: "MMA News Weekly",
-    image: "/images/thumbnail7.jpg",
-  },
-  {
-    articleId: 8,
-    name: "Amanda Nunes Retires from MMA, Cites Desire to Pursue Boxing Career",
-    publishedDate: "55 minutes ago",
-    source: "Combat Sports Gazette",
-    image: "/images/thumbnail8.jpg",
-  },
-  {
-    articleId: 9,
-    name: "Nate Diaz Announces Return to Octagon, Targets Showdown with Conor McGregor",
-    publishedDate: "50 minutes ago",
-    source: "Fighter's Realm",
-    image: "/images/thumbnail9.jpg",
-  },
-  {
-    articleId: 10,
-    name: "UFC 301 Officially Announced: McGregor vs. Ferguson Set as Main Event",
-    publishedDate: "30 minutes ago",
-    source: "ESPN MMA",
-    image: "/images/thumbnail10.jpg",
-  },
-  {
-    articleId: 11,
-    name: "Bellator Signs Rising Prospect to Multi-Fight Deal",
-    publishedDate: "1 day ago",
-    source: "Bellator MMA",
-    image: "/images/thumbnail11.jpg",
-  },
-  {
-    articleId: 12,
-    name: "Rousey Makes Surprise Appearance at WWE WrestleMania",
-    publishedDate: "5 hours ago",
-    source: "WWE",
-    image: "/images/thumbnail12.jpg",
-  },
-  {
-    articleId: 13,
-    name: "Fury vs. Joshua Mega-Fight Confirmed for Summer 2024",
-    publishedDate: "Yesterday",
-    source: "Boxing Insider",
-    image: "/images/thumbnail13.jpg",
-  },
-  {
-    articleId: 14,
-    name: "McGregor Launches New Clothing Line in Collaboration with Gucci",
-    publishedDate: "2 hours ago",
-    source: "Fashion Insider",
-    image: "/images/thumbnail14.jpg",
-  },
-  {
-    articleId: 15,
-    name: "Holm vs. Cyborg Rematch Set for UFC 302",
-    publishedDate: "3 hours ago",
-    source: "MMA Weekly",
-    image: "/images/thumbnail15.jpg",
-  },
-  {
-    articleId: 16,
-    name: "UFC Announces Plans for Expansion into Africa",
-    publishedDate: "4 hours ago",
-    source: "UFC Africa",
-    image: "/images/thumbnail16.jpg",
-  },
-  {
-    articleId: 17,
-    name: "Miocic vs. Jones Confirmed for Heavyweight Championship",
-    publishedDate: "Yesterday",
-    source: "UFC News Hub",
-    image: "/images/thumbnail17.jpg",
-  },
-  {
-    articleId: 18,
-    name: "Prime Drink Comes Out with UFC 300 Flavor",
-    publishedDate: "1 hour ago",
-    source: "MMA Fighting",
-    image: "/images/thumbnail18.jpg",
-  },
+	{
+		articleId: 1,
+		name: "Topuria KOs Volkanovski to win the FW title",
+		publishedDate: "3 days ago",
+		source: "UFC",
+		image: "/images/thumbnail1.jpg",
+	},
+	{
+		articleId: 2,
+		name: "Pereira vs Hill announced to be main event of UFC 300",
+		publishedDate: "2 days ago",
+		source: "MMA Junkie",
+		image: "/images/thumbnail2.jpg",
+	},
+	{
+		articleId: 3,
+		name: "O'Malley targets Topuria to be his next fight",
+		publishedDate: "1 hour ago",
+		source: "ONE FC",
+		image: "/images/thumbnail3.jpg",
+	},
+	{
+		articleId: 4,
+		name: "Jones to fight Namajunas in 2024 in spite of torn pectorial",
+		publishedDate: "Yesterday",
+		source: "Sherdog",
+		image: "/images/thumbnail4.jpg",
+	},
+	{
+		articleId: 5,
+		name: "Royval defeats Moreno",
+		publishedDate: "2 days ago",
+		source: "MMA Frenzy",
+		image: "/images/thumbnail5.jpg",
+	},
+	{
+		articleId: 6,
+		name: "Rodtang to train with 10th Planet Jiu-Jitsu",
+		publishedDate: "1 hour ago",
+		source: "ONE FC",
+		image: "/images/thumbnail6.jpg",
+	},
+	{
+		articleId: 7,
+		name: "GSP Returns: Georges St-Pierre Confirms Comeback Fight Against Khabib Nurmagomedov",
+		publishedDate: "1 hour ago",
+		source: "MMA News Weekly",
+		image: "/images/thumbnail7.jpg",
+	},
+	{
+		articleId: 8,
+		name: "Amanda Nunes Retires from MMA, Cites Desire to Pursue Boxing Career",
+		publishedDate: "55 minutes ago",
+		source: "Combat Sports Gazette",
+		image: "/images/thumbnail8.jpg",
+	},
+	{
+		articleId: 9,
+		name: "Nate Diaz Announces Return to Octagon, Targets Showdown with Conor McGregor",
+		publishedDate: "50 minutes ago",
+		source: "Fighter's Realm",
+		image: "/images/thumbnail9.jpg",
+	},
+	{
+		articleId: 10,
+		name: "UFC 301 Officially Announced: McGregor vs. Ferguson Set as Main Event",
+		publishedDate: "30 minutes ago",
+		source: "ESPN MMA",
+		image: "/images/thumbnail10.jpg",
+	},
+	{
+		articleId: 11,
+		name: "Bellator Signs Rising Prospect to Multi-Fight Deal",
+		publishedDate: "1 day ago",
+		source: "Bellator MMA",
+		image: "/images/thumbnail11.jpg",
+	},
+	{
+		articleId: 12,
+		name: "Rousey Makes Surprise Appearance at WWE WrestleMania",
+		publishedDate: "5 hours ago",
+		source: "WWE",
+		image: "/images/thumbnail12.jpg",
+	},
+	{
+		articleId: 13,
+		name: "Fury vs. Joshua Mega-Fight Confirmed for Summer 2024",
+		publishedDate: "Yesterday",
+		source: "Boxing Insider",
+		image: "/images/thumbnail13.jpg",
+	},
+	{
+		articleId: 14,
+		name: "McGregor Launches New Clothing Line in Collaboration with Gucci",
+		publishedDate: "2 hours ago",
+		source: "Fashion Insider",
+		image: "/images/thumbnail14.jpg",
+	},
+	{
+		articleId: 15,
+		name: "Holm vs. Cyborg Rematch Set for UFC 302",
+		publishedDate: "3 hours ago",
+		source: "MMA Weekly",
+		image: "/images/thumbnail15.jpg",
+	},
+	{
+		articleId: 16,
+		name: "UFC Announces Plans for Expansion into Africa",
+		publishedDate: "4 hours ago",
+		source: "UFC Africa",
+		image: "/images/thumbnail16.jpg",
+	},
+	{
+		articleId: 17,
+		name: "Miocic vs. Jones Confirmed for Heavyweight Championship",
+		publishedDate: "Yesterday",
+		source: "UFC News Hub",
+		image: "/images/thumbnail17.jpg",
+	},
+	{
+		articleId: 18,
+		name: "Prime Drink Comes Out with UFC 300 Flavor",
+		publishedDate: "1 hour ago",
+		source: "MMA Fighting",
+		image: "/images/thumbnail18.jpg",
+	},
 ];
 
 export const filterItems = {
-  all: "all",
-  article: "article",
-  fighter: "fighter",
-  event: "event",
+	all: "all",
+	article: "article",
+	fighter: "fighter",
+	event: "event",
+};
+
+export const divisions = {
+	ufc: {
+		men: {
+			Flyweight: "125 lbs (56.7 kg)",
+			Bantamweight: "135 lbs (61.2 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "265 lbs (120.2 kg)",
+		},
+		women: {
+			Strawweight: "115 lbs (52.2 kg)",
+			Flyweight: "125 lbs (56.7 kg)",
+			Bantamweight: "135 lbs (61.2 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+		},
+	},
+	onefc: {
+		men: {
+			Strawweight: "115 lbs (52.2 kg)",
+			Flyweight: "125 lbs (56.7 kg)",
+			Bantamweight: "135 lbs (61.2 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "Unlimited",
+		},
+		women: {
+			Atomweight: "105 lbs (47.6 kg)",
+			Strawweight: "115 lbs (52.2 kg)",
+			Flyweight: "125 lbs (56.7 kg)",
+			Bantamweight: "135 lbs (61.2 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "Unlimited",
+		},
+	},
+	bellator: {
+		men: {
+			Bantamweight: "135 lbs (61.2 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "Unlimited",
+		},
+		women: {
+			Flyweight: "125 lbs (56.7 kg)",
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "Unlimited",
+		},
+	},
+	pfl: {
+		men: {
+			Featherweight: "145 lbs (65.8 kg)",
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+			Middleweight: "185 lbs (83.9 kg)",
+			"Light Heavyweight": "205 lbs (93.0 kg)",
+			Heavyweight: "Unlimited",
+		},
+		women: {
+			Lightweight: "155 lbs (70.3 kg)",
+			Welterweight: "170 lbs (77.1 kg)",
+		},
+	},
+};
+
+export const p4p = {
+  ufc: [
+    { 
+      fighterName: "Kamaru Usman",
+      weightClass: "Welterweight",
+      record: "19-1",
+      nationality: "Nigeria",
+      age: 34
+    },
+    { 
+      fighterName: "Israel Adesanya",
+      weightClass: "Middleweight",
+      record: "21-1",
+      nationality: "Nigeria",
+      age: 32
+    },
+    { 
+      fighterName: "Amanda Nunes",
+      weightClass: "Bantamweight/Featherweight",
+      record: "21-4",
+      nationality: "Brazil",
+      age: 33
+    },
+    { 
+      fighterName: "Valentina Shevchenko",
+      weightClass: "Flyweight",
+      record: "21-3",
+      nationality: "Kyrgyzstan",
+      age: 33
+    },
+    { 
+      fighterName: "Francis Ngannou",
+      weightClass: "Heavyweight",
+      record: "16-3",
+      nationality: "Cameroon",
+      age: 35
+    }
+  ],
+  bellator: [
+    { 
+      fighterName: "Patricio Pitbull",
+      weightClass: "Featherweight/Lightweight",
+      record: "32-4",
+      nationality: "Brazil",
+      age: 34
+    },
+    { 
+      fighterName: "Douglas Lima",
+      weightClass: "Welterweight",
+      record: "32-9",
+      nationality: "Brazil",
+      age: 33
+    },
+    { 
+      fighterName: "Cris Cyborg",
+      weightClass: "Featherweight",
+      record: "24-2",
+      nationality: "Brazil",
+      age: 36
+    },
+    { 
+      fighterName: "A.J. McKee",
+      weightClass: "Featherweight",
+      record: "18-0",
+      nationality: "USA",
+      age: 26
+    }
+  ],
+  onefc: [
+    { 
+      fighterName: "Aung La Nsang",
+      weightClass: "Light Heavyweight/Middleweight",
+      record: "27-11",
+      nationality: "Myanmar",
+      age: 36
+    },
+    { 
+      fighterName: "Eddie Alvarez",
+      weightClass: "Lightweight",
+      record: "30-8",
+      nationality: "USA",
+      age: 37
+    },
+    { 
+      fighterName: "Christian Lee",
+      weightClass: "Lightweight",
+      record: "15-3",
+      nationality: "Singapore",
+      age: 23
+    },
+    { 
+      fighterName: "Angela Lee",
+      weightClass: "Atomweight",
+      record: "11-2",
+      nationality: "USA",
+      age: 25
+    }
+  ],
+  pfl: [
+    { 
+      fighterName: "Kayla Harrison",
+      weightClass: "Lightweight",
+      record: "11-0",
+      nationality: "USA",
+      age: 31
+    },
+    { 
+      fighterName: "Lance Palmer",
+      weightClass: "Featherweight",
+      record: "22-4",
+      nationality: "USA",
+      age: 34
+    },
+    { 
+      fighterName: "Ray Cooper III",
+      weightClass: "Welterweight",
+      record: "21-7-1",
+      nationality: "USA",
+      age: 28
+    },
+    { 
+      fighterName: "Natan Schulte",
+      weightClass: "Lightweight",
+      record: "20-4-1",
+      nationality: "Brazil",
+      age: 30
+    }
+  ]
 };
 
 export const savedItems = {
-  items: [
-    {
-      type: "article",
-      name: "Jones vs. Miocic Set For April 2024",
-      added: "2024-03-17T08:00:00", // Example time: 08:00:00 (8:00 AM)
-    },
-    {
-      type: "fighter",
-      name: "Islam Makhachev",
-      image: "/images/fighterIslamMakhachev.jpg",
-      added: "2024-03-17T12:30:00", // Example time: 12:30:00 (12:30 PM)
-    },
-    {
-      type: "event",
-      name: "UFC 300",
-      location: "T-Mobile Arena in Las Vegas, NV",
-      added: "2024-03-17T15:45:00", // Example time: 15:45:00 (3:45 PM)
-    },
-    {
-      type: "event",
-      name: "UFC 299",
-      location: "Kaseya Center in Miami, FL",
-      added: "2024-03-17T18:20:00", // Example time: 18:20:00 (6:20 PM)
-    },
-    {
-      type: "event",
-      name: "ONE Friday Fights 55",
-      location: "Lumpinee Boxing Stadium in Bangkok, Thailand",
-      added: "2024-03-17T21:00:00", // Example time: 21:00:00 (9:00 PM)
-    },
-  ],
+	items: [
+		{
+			type: "article",
+			name: "Jones vs. Miocic Set For April 2024",
+			added: "2024-03-17T08:00:00", // Example time: 08:00:00 (8:00 AM)
+		},
+		{
+			type: "fighter",
+			name: "Islam Makhachev",
+			image: "/images/fighterIslamMakhachev.jpg",
+			added: "2024-03-17T12:30:00", // Example time: 12:30:00 (12:30 PM)
+		},
+		{
+			type: "event",
+			name: "UFC 300",
+			location: "T-Mobile Arena in Las Vegas, NV",
+			added: "2024-03-17T15:45:00", // Example time: 15:45:00 (3:45 PM)
+		},
+		{
+			type: "event",
+			name: "UFC 299",
+			location: "Kaseya Center in Miami, FL",
+			added: "2024-03-17T18:20:00", // Example time: 18:20:00 (6:20 PM)
+		},
+		{
+			type: "event",
+			name: "ONE Friday Fights 55",
+			location: "Lumpinee Boxing Stadium in Bangkok, Thailand",
+			added: "2024-03-17T21:00:00", // Example time: 21:00:00 (9:00 PM)
+		},
+	],
 };
 
 export const userAccountOptions = {
-  changeEmail: "Change Email",
-  changePassword: "Change Password",
-  logout: "Logout",
-  deleteAccount: "Delete Account",
+	changeEmail: "Change Email",
+	changePassword: "Change Password",
+	logout: "Logout",
+	deleteAccount: "Delete Account",
 };
 
 export const users = [
-  {
-    id: 1,
-    username: "user1",
-    email: "user1@example.com",
-    password: "password123",
-    fullName: "John Doe",
-    age: 25,
-    gender: "male",
-    createdAt: "2024-03-12T08:00:00Z",
-    updatedAt: "2024-03-12T08:00:00Z",
-  },
-  {
-    id: 2,
-    username: "user2",
-    email: "user2@example.com",
-    password: "password456",
-    fullName: "Jane Smith",
-    age: 30,
-    gender: "female",
-    createdAt: "2024-03-12T08:00:00Z",
-    updatedAt: "2024-03-12T08:00:00Z",
-  },
-  {
-    id: 3,
-    username: "user3",
-    email: "user3@example.com",
-    password: "password789",
-    fullName: "Michael Johnson",
-    age: 35,
-    gender: "male",
-    createdAt: "2024-03-12T08:00:00Z",
-    updatedAt: "2024-03-12T08:00:00Z",
-  },
-  {
-    id: 4,
-    username: "user4",
-    email: "user4@example.com",
-    password: "passwordabc",
-    fullName: "Emily Brown",
-    age: 28,
-    gender: "female",
-    createdAt: "2024-03-12T08:00:00Z",
-    updatedAt: "2024-03-12T08:00:00Z",
-  },
+	{
+		id: 1,
+		username: "user1",
+		email: "user1@example.com",
+		password: "password123",
+		fullName: "John Doe",
+		age: 25,
+		gender: "male",
+		createdAt: "2024-03-12T08:00:00Z",
+		updatedAt: "2024-03-12T08:00:00Z",
+	},
+	{
+		id: 2,
+		username: "user2",
+		email: "user2@example.com",
+		password: "password456",
+		fullName: "Jane Smith",
+		age: 30,
+		gender: "female",
+		createdAt: "2024-03-12T08:00:00Z",
+		updatedAt: "2024-03-12T08:00:00Z",
+	},
+	{
+		id: 3,
+		username: "user3",
+		email: "user3@example.com",
+		password: "password789",
+		fullName: "Michael Johnson",
+		age: 35,
+		gender: "male",
+		createdAt: "2024-03-12T08:00:00Z",
+		updatedAt: "2024-03-12T08:00:00Z",
+	},
+	{
+		id: 4,
+		username: "user4",
+		email: "user4@example.com",
+		password: "passwordabc",
+		fullName: "Emily Brown",
+		age: 28,
+		gender: "female",
+		createdAt: "2024-03-12T08:00:00Z",
+		updatedAt: "2024-03-12T08:00:00Z",
+	},
 ];
