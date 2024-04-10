24e4906bc0deb07e074b1df817837ce882bd799f
Arhon Pineda <aronp2000@gmail.com>
2024-04-09 21:04:59 -0500

    "Update GET request in fighters route to compare API data with database data"

--- app/api/ufc/fighters/route.js
+++ app/api/ufc/fighters/route.js
@@ -6,5 +6,20 @@ import { fetchAPI } from "@/scripts/util";
 connectToDatabase()
 
 export async function GET() {
-	
+	try {
+    const apiData = await fetchAPI("https://api.octagon-api.com/fighters");
+    const dbData = await UfcFighter.find({});
+    let responseMessage = ""
+    const fighterIds = Object.keys(apiData)
+    const dbFighterIds = Object.keys(dbData)
+
+    // Check if both arrs have same length and same value
+    if (fighterIds.length === dbFighterIds.length && fighterIds.every((value, index) => value === dbData[index])) {
+
+    }
+
+    return NextResponse.json({message: responseMessage, content: apiData});
+  } catch (error) {
+    return NextResponse.json({message: error.message});
+  }
 }
