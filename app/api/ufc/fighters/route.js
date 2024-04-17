b1ce74748f56b4620e254a3c022dc55a40eac04f
Arhon Pineda <aronp2000@gmail.com>
2024-04-16 19:38:01 -0500

    update db data format, add unit tests

--- app/api/ufc/fighters/route.js
+++ app/api/ufc/fighters/route.js
@@ -1,64 +1,65 @@
 import UfcFighter from "@/lib/mongodb/models/UfcFighter";
 import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/mongodb/connect";
-import { fetchAPI, getCountryName } from "@/scripts/util";
-import countries from "country-codes-list"
+import { fetchAPI } from "@/scripts/util";
+import countries from "country-codes-list";
+import { getCountryName } from "@/scripts/util";
 
 connectToDatabase();
 
+const countryCodes = countries.customList("countryNameEn", "{countryCode}");
+
 function areEqual(prevWins, newWins) {
-	if (prevWins.length !== newWins.length) {
-		return false;
-	}
-	return prevWins.every((element, index) => element === newWins[index]);
+  if (prevWins.length !== newWins.length) {
+    return false;
+  }
+  return prevWins.every((element, index) => element === newWins[index]);
 }
 
-	// Assign fetched key names to new 'id' field
-	// Assign fetched values to new 'details' field
+// Assign fetched keys to new 'id' field
+// Assign fetched values to new 'details' key
 function reformat(keysArr, apiDataObj) {
-	let transformedData = {};
-	keysArr.forEach((keyName) => {
-		let fighterDetails = {};
-		fighterDetails.id = keyName;
-		fighterDetails.details = apiDataObj[keyName];
-		transformedData[keyName] = fighterDetails;
-	});
-	return Object.values(transformedData);
+  let transformedData = {};
+  keysArr.forEach((keyName) => {
+    let fighterDetails = {};
+    fighterDetails.id = keyName;
+    fighterDetails.details = apiDataObj[keyName];
+    fighterDetails.details.flag =
+      countryCodes[getCountryName(fighterDetails.details.placeOfBirth)];
+    transformedData[keyName] = fighterDetails;
+  });
+  return Object.values(transformedData);
 }
 
 export async function GET() {
-	try {
-		const apiData = await fetchAPI("https://api.octagon-api.com/fighters");
-		let responseMessage = "";
+  try {
+    const apiData = await fetchAPI("https://api.octagon-api.com/fighters");
+    let responseMessage = "";
 
-		const fighterIds = Object.keys(apiData);
+    const fighterIds = Object.keys(apiData);
 
-		const newData = reformat(fighterIds, apiData);
-		const oldData = await UfcFighter.find({});
+    const newData = reformat(fighterIds, apiData);
+    const oldData = await UfcFighter.find({});
 
-		const prevFighterData = oldData.map((obj) => obj.details.wins);
-		const nextFighterData = newData.map((obj) => obj.details.wins);
+    const prevFighterData = oldData.map((obj) => obj.details.wins);
+    const nextFighterData = newData.map((obj) => obj.details.wins);
 
-		if (!areEqual(prevFighterData, nextFighterData)) {
-			await UfcFighter.deleteMany({});
-			await UfcFighter.insertMany(newData);
-			responseMessage = "Collection updated: " + UfcFighter.modelName;
-		} else {
-			responseMessage = "Data is up-to-date: " + UfcFighter.modelName;
-		}
+    if (!areEqual(prevFighterData, nextFighterData)) {
+      await UfcFighter.deleteMany({});
+      await UfcFighter.insertMany(newData);
+      responseMessage = "Collection updated: " + UfcFighter.modelName;
+    } else {
+      responseMessage = "Data is up-to-date: " + UfcFighter.modelName;
+    }
 
-		return NextResponse.json(
-			{ message: responseMessage, content: await UfcFighter.find({}) },
-			{ statusText: 200 }
-		);
-	} catch (error) {
-		return NextResponse.json(
-			{ message: "An error occured: " + error.message },
-			{ statusText: 500 }
-		);
-	}
-} 
-
-const countryObj = countries.customList("countryNameEn", "{countryCode}")
-
-console.log(countryObj["United Kingdom"])
+    return NextResponse.json(
+      { message: responseMessage, content: await UfcFighter.find({}) },
+      { statusText: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occured: " + error.message },
+      { statusText: 500 }
+    );
+  }
+}
--- scripts/util.js
+++ scripts/util.js
@@ -1,32 +1,62 @@
-import axios from "axios";
-import { countryNames } from "@/constants";
-import countries from "country-codes-list"
+const axios = require("axios");
 
-export function decodeUrl(urlEncodedString) {
-  return decodeURIComponent(urlEncodedString.replace(/%([0-9A-Fa-f]{2})/g, (match, p1) => String.fromCharCode(parseInt(p1, 16))));
+function decodeUrl(urlEncodedString) {
+  return decodeURIComponent(
+    urlEncodedString.replace(/%([0-9A-Fa-f]{2})/g, (match, p1) =>
+      String.fromCharCode(parseInt(p1, 16))
+    )
+  );
 }
 
-export async function fetchAPI(url) {
+/**
+ *
+ * @param {string} url - An API endpoint
+ * @returns {AxiosResponse<any, any>} The response
+ */
+async function fetchAPI(url) {
   try {
-		const response = await axios.get(url);
-		if (!response.status === 200) {
-			throw new Error("Failed to fetch: ", url);
-		}
-		return response.data;
-	} catch (error) {
-		console.error("Error fetching: ", error);
-		throw error;
-	}
+    const response = await axios.get(url);
+    if (!response.status === 200) {
+      throw new Error("Failed to fetch: ", url);
+    }
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching: ", error);
+    throw error;
+  }
+}
+/**
+ * @param {string} location Fighter's place of birth
+ * @returns {string} Fighter's country name
+ */
+function getCountryName(location = "") {
+  let countryName = "";
+  const target = location.lastIndexOf(", ");
+
+  if (target > -1) {
+    // target has comma
+    countryName = location.substring(target + 2);
+  } else {
+    // no comma
+    if (location === undefined || location === null || location === "") {
+      countryName = "";
+    } else {
+      countryName = location;
+    }
+  }
+
+  switch (countryName) {
+    case "United States":
+      countryName += " of America";
+      break;
+    case "England":
+      countryName = "United Kingdom";
+      break;
+    default:
+      break;
+  }
+
+  return countryName;
 }
 
-export function getCountryName(location) {
-	const commaIndex = location.indexOf(', ');
-	if (commaIndex > -1) {
-			console.log(location.substring(commaIndex + 2))
-			return location.substring(commaIndex + 2);
-	} else {
-			console.log(location)
-			return location;
-	}
-}
-
+module.exports = { fetchAPI, getCountryName, decodeUrl };
